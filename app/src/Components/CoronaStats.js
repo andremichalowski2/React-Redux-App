ad0ea6a9ba5e5bbe77f5de858daf0a569129c4d9
amichalo421 <andre.michalowski@gmail.com>
2020-09-10 18:49:00 -0400

    Update (simplify) components

--- app/src/Components/CoronaStats.js
+++ app/src/Components/CoronaStats.js
@@ -1,27 +1,43 @@
+// import React from 'react';
+
+// import CoronaStat from './CoronaStat';
+
+// import { connect } from 'react-redux';
+
+// const CoronaStats = ({jokes}) => {
+//   console.log('state coming from statS', jokes)
+//   return(
+//     <div>
+//       <h3>Title: CoronaStats Component</h3>
+//         <CoronaStat />
+//         {jokes.map((j) => {
+//           return <CoronaStat joke={j} key={j.id}/>
+//         })}
+//     </div>
+//   )
+// }
+
+// function mapStateToProps(state) {
+//   // console.log(state)
+//   return {
+//     jokes: state
+//   }
+// }
+
+// export default connect(mapStateToProps, {})(CoronaStats);
+
+
 import React from 'react';
 
-import CoronaStat from './CoronaStat';
-
-import { connect } from 'react-redux';
-
-const CoronaStats = ({jokes}) => {
-  console.log('state coming from statS', jokes)
-  return(
-    <div>
-      <h3>Title: CoronaStats Component</h3>
-        <CoronaStat />
-        {jokes.map((j) => {
-          return <CoronaStat joke={j} key={j.id}/>
-        })}
-    </div>
-  )
+const CoronaStats = (props) => {
+    return (
+        <div className = 'card'>
+        <h2>{props.title}</h2>
+        <p className = 'cases'>{props.cases}</p>
+        <p className = 'date'>{props.date}</p>
+    <p className = 'description'>Number of {props.description} of Corona Virus.</p>
+        </div>
+    )
 }
 
-function mapStateToProps(state) {
-  // console.log(state)
-  return {
-    jokes: state
-  }
-}
-
-export default connect(mapStateToProps, {})(CoronaStats);
+export default CoronaStats;
